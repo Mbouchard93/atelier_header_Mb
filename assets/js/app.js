12035f531b5d3d9d8e39109cc39b2f77fda06beb
Mbouchard93 <mariiane.bouchard12@gmail.com>
2024-04-26 13:42:23 -0400

    aria-label

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,16 @@
 const icone = document.querySelector('.icone');
 const overlay = document.querySelector('.modal');
 const burger = document.querySelector('.mobile_display');
+const navDesktop = document.querySelector('.nav_desktop');
 
 icone.addEventListener('click', () => {
     overlay.classList.add('hidden');
+    overlay.setAttribute('aria-label', 'hidden');
 } );
 
 
 
 burger.addEventListener('click', () => {
     overlay.classList.remove('hidden');
-})
+    overlay.removeAttribute('aria-label');
+});
