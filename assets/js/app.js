0484097c6e2c07dd7b8df34bbef6d8f83e821b05
Mbouchard93 <mariiane.bouchard12@gmail.com>
2024-04-28 14:49:57 -0400

    accessibilité

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,12 +5,36 @@ const navDesktop = document.querySelector('.nav_desktop');
 
 icone.addEventListener('click', () => {
     overlay.classList.add('hidden');
-    overlay.setAttribute('aria-label', 'hidden');
+    overlay.setAttribute('aria-hidden', 'true');
+
 } );
-
-
 
 burger.addEventListener('click', () => {
     overlay.classList.remove('hidden');
-    overlay.removeAttribute('aria-label');
+    overlay.setAttribute('aria-hidden', 'false');
+    navDesktop.setAttribute('aria-hidden', 'true');
+
 });
+
+
+function aria ()  {
+    if (overlay.getAttribute('aria-hidden') === 'true'){
+        navDesktop.setAttribute('aria-hidden', 'false'); 
+    } else {
+        navDesktop.removeAttribute('aria-hidden');
+    }
+};
+
+
+function visible() {
+    const style = window.getComputedStyle(navDesktop);
+    if (style.visibility === 'visible'){
+        navDesktop.setAttribute('aria-hidden', 'false'); 
+    } else {
+        navDesktop.setAttribute('aria-hidden', 'true');
+    }
+}
+
+window.addEventListener('resize', visible) ;
+
+
